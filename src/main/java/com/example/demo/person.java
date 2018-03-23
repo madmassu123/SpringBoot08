331f9e1b69c0d564a3de70a8e37bfe14d45eefc9
GBTC440012UR <GBTC440012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-23 09:14:47 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/person.java
@@ -0,0 +1,46 @@
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+    @Entity
+    public class person{
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long id;
+        private String firstName;
+        private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
+
+
+
+
+
+
+
